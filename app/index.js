dd8a965befcf6396a1c6907ab2ea59c48dd9bc00
Victor Zidaroiu <victor@zidaroiu.com>
2016-12-28 14:15:13 +0000

    Update port

--- app/index.js
+++ app/index.js
@@ -13,5 +13,5 @@ app.use('/query', graphqlHTTP({
 
 app.listen(process.env.PORT);
 
-console.log(`Server started on port ${4000}`);
+console.log(`Server started on port ${process.env.PORT}`);
 console.log('GraphQL endpoint: /query');
